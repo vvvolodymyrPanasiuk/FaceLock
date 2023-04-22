f9c8fcdc94212c6a65fdb37a316dcbbd3bb785f9
VolodymyrPanasiuk <vovkawool@gmail.com>
2023-04-23 00:21:42 +0300

    Documantation data management service

--- Services/FaceLock.DataManagement/Services/IDoorLockService.cs
+++ Services/FaceLock.DataManagement/Services/IDoorLockService.cs
@@ -2,26 +2,111 @@
 
 namespace FaceLock.DataManagement.Services
 {
+    /// <summary>
+    /// Interface to manage door lock data that interacts with the door lock aggregate repositories through the unit of work.
+    /// </summary>
     public interface IDoorLockService
     {
+        /// <summary>
+        /// Gets all the door lock accesses for a user by user ID.
+        /// </summary>
+        /// <param name="userId">The ID of the user.</param>
+        /// <returns>An IEnumerable of UserDoorLockAccess objects representing the door lock accesses for the user.</returns>
         Task<IEnumerable<UserDoorLockAccess>> GetAccessByUserIdAsync(string userId);
+        /// <summary>
+        /// Gets all the door lock accesses for a door lock by door lock ID.
+        /// </summary>
+        /// <param name="doorLockId">The ID of the door lock.</param>
+        /// <returns>An IEnumerable of UserDoorLockAccess objects representing the door lock accesses for the door lock.</returns>
         Task<IEnumerable<UserDoorLockAccess>> GetAccessByDoorLockIdAsync(int doorLockId);
+        /// <summary>
+        /// Gets all the access tokens for a door lock by door lock ID.
+        /// </summary>
+        /// <param name="doorLockId">The ID of the door lock.</param>
+        /// <returns>An IEnumerable of DoorLockAccessToken objects representing the access tokens for the door lock.</returns>
         Task<IEnumerable<DoorLockAccessToken>> GetAccessTokenByDoorLockIdAsync(int doorLockId);
+        /// <summary>
+        /// Gets all the door lock history records for a user by user ID.
+        /// </summary>
+        /// <param name="userId">The ID of the user.</param>
+        /// <returns>An IEnumerable of DoorLockHistory objects representing the door lock history records for the user.</returns>
         Task<IEnumerable<DoorLockHistory>> GetDoorLockHistoryByUserIdAsync(string userId);
+        /// <summary>
+        /// Gets all the door lock history records for a door lock by door lock ID.
+        /// </summary>
+        /// <param name="doorLockId">The ID of the door lock.</param>
+        /// <returns>An IEnumerable of DoorLockHistory objects representing the door lock history records for the door lock.</returns>
         Task<IEnumerable<DoorLockHistory>> GetDoorLockHistoryByDoorLockIdAsync(int doorLockId);
+        /// <summary>
+        /// Gets a door lock by ID.
+        /// </summary>
+        /// <param name="doorLockId">The ID of the door lock.</param>
+        /// <returns>A DoorLock object representing the door lock.</returns>
         Task<DoorLock> GetDoorLockByIdAsync(int doorLockId);
+        /// <summary>
+        /// Gets all the door locks.
+        /// </summary>
+        /// <returns>An IEnumerable of DoorLock objects representing all the door locks.</returns>
         Task<IEnumerable<DoorLock>> GetAllDoorLocksAsync();
+        /// <summary>
+        /// Adds a new door lock.
+        /// </summary>
+        /// <param name="doorLock">The DoorLock object representing the new door lock.</param>
         Task AddDoorLockAsync(DoorLock doorLock);
+        /// <summary>
+        /// Updates a door lock.
+        /// </summary>
+        /// <param name="doorLock">The DoorLock object representing the updated door lock.</param>
         Task UpdateDoorLockAsync(DoorLock doorLock);
+        /// <summary>
+        /// Deletes a door lock.
+        /// </summary>
+        /// <param name="doorLock">The DoorLock object representing the door lock to be deleted.</param>
         Task DeleteDoorLockAsync(DoorLock doorLock);
+        /// <summary>
+        /// Adds a new door lock access for user.
+        /// </summary>
+        /// <param name="userDoorLockAccess">The UserDoorLockAccess object representing the new door lock access.</param>
         Task AddDoorLockAccessAsync(UserDoorLockAccess userDoorLockAccess);
+        /// <summary>
+        /// Updates a door lock access for user.
+        /// </summary>
+        /// <param name="userDoorLockAccess">The UserDoorLockAccess object representing the updated door lock access.</param>
         Task UpdateDoorLockAccessAsync(UserDoorLockAccess userDoorLockAccess);
+        /// <summary>
+        /// Deletes a door lock access for user.
+        /// </summary>
+        /// <param name="userDoorLockAccess">The UserDoorLockAccess object to delete.</param>
         Task DeleteDoorLockAccessAsync(UserDoorLockAccess userDoorLockAccess);
-        Task AddDoorLockAccessTokenAsync(DoorLockAccessToken doorLockAccessToken);
-        Task UpdateDoorLockAccessTokenAsync(DoorLockAccessToken doorLockAccessToken);
-        Task DeleteDoorLockAccessTokenAsync(DoorLockAccessToken doorLockAccessToken);
+        /// <summary>
+        /// Adds a door lock accesses for users.
+        /// </summary>
+        /// <param name="userDoorLockAccess">The UserDoorLockAccess objects to add.</param>
+        Task AddDoorLockAccessesAsync(IEnumerable<UserDoorLockAccess> userDoorLockAccess);
+        /// <summary>
+        /// Updates a door lock accesses for users.
+        /// </summary>
+        /// <param name="userDoorLockAccess">The UserDoorLockAccess objects to update.</param>
+        Task UpdateDoorLockAccessesAsync(IEnumerable<UserDoorLockAccess> userDoorLockAccess);
+        /// <summary>
+        /// Deletes a door lock accesses for users.
+        /// </summary>
+        /// <param name="userDoorLockAccess">The UserDoorLockAccess objects to delete.</param>
+        Task DeleteDoorLockAccessesAsync(IEnumerable<UserDoorLockAccess> userDoorLockAccess);
+        /// <summary>
+        /// Adds a door lock history for user.
+        /// </summary>
+        /// <param name="doorLockHistory">The DoorLockHistory object to add.</param>
         Task AddDoorLockHistoryAsync(DoorLockHistory doorLockHistory);
+        /// <summary>
+        /// Updates a door lock history for user.
+        /// </summary>
+        /// <param name="doorLockHistory">The DoorLockHistory object to update.</param>
         Task UpdateDoorLockHistoryAsync(DoorLockHistory doorLockHistory);
+        /// <summary>
+        /// Deletes a door lock history for user.
+        /// </summary>
+        /// <param name="doorLockHistory">The DoorLockHistory object to delete.</param>
         Task DeleteDoorLockHistoryAsync(DoorLockHistory doorLockHistory);
     }
 }
--- Services/FaceLock.DataManagement/Services/IUserService.cs
+++ Services/FaceLock.DataManagement/Services/IUserService.cs
@@ -2,16 +2,58 @@
 
 namespace FaceLock.DataManagement.Services
 {
+    /// <summary>
+    /// Interface to manage user data that interacts with the user aggregate repositories through the unit of work.
+    /// </summary>
     public interface IUserService
     {
+        /// <summary>
+        /// Gets a User entity by user ID.
+        /// </summary>
+        /// <param name="userId">The ID of the User entity to retrieve.</param>
+        /// <returns>The User entity with the specified ID.</returns>
         Task<User> GetUserByIdAsync(string userId);
+        /// <summary>
+        /// Gets a User entity by user username.
+        /// </summary>
+        /// <param name="userName">The username of the User entity to retrieve.</param>
+        /// <returns>The User entity with the specified username.</returns>
         Task<User> GetUserByUsernameAsync(string userName);
+        /// <summary>
+        /// Gets all UserFace entities for a User entity.
+        /// </summary>
+        /// <param name="userId">The ID of the User entity to retrieve UserFace entities for.</param>
+        /// <returns>An IEnumerable of UserFace entities for the specified User entity.</returns>
         Task<IEnumerable<UserFace>> GetAllUserFacesAsync(string userId);
+        /// <summary>
+        /// Adds a new User entity to the data store.
+        /// </summary>
+        /// <param name="user">The User entity to add.</param>
         Task AddUserAsync(User user);
+        /// <summary>
+        /// Updates an existing User entity in the data store.
+        /// </summary>
+        /// <param name="user">The User entity to update.</param>
         Task UpdateUserAsync(User user);
+        /// <summary>
+        /// Deletes an existing User entity from the data store.
+        /// </summary>
+        /// <param name="user">The User entity to delete.</param>
         Task DeleteUserAsync(User user);
+        /// <summary>
+        /// Adds a new UserFace entity to the data store.
+        /// </summary>
+        /// <param name="userFace">The UserFace entity to add.</param>
         Task AddUserFaceAsync(UserFace userFace);
-        Task UpdateUserFaceAsync(UserFace userFace);
+        /// <summary>
+        /// Adds multiple new UserFace entities to the data store.
+        /// </summary>
+        /// <param name="userFaces">An IEnumerable of UserFace entities to add.</param>
+        Task AddUserFacesAsync(IEnumerable<UserFace> userFace);
+        /// <summary>
+        /// Deletes an existing UserFace entity from the data store.
+        /// </summary>
+        /// <param name="userFace">The UserFace entity to delete.</param>
         Task DeleteUserFaceAsync(UserFace userFace);
     }
 }
