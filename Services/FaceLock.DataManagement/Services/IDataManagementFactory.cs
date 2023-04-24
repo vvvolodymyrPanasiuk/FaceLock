d56f0a8a9284f472a712c9beb60de72acb2cc651
VolodymyrPanasiuk <vovkawool@gmail.com>
2023-04-24 16:49:55 +0300

    Implement IDataServiceFactory using abstract factory pattern

---
+++ Services/FaceLock.DataManagement/Services/IDataManagementFactory.cs
@@ -0,0 +1,47 @@
+﻿using FaceLock.DataManagement.Services.Commands;
+using FaceLock.DataManagement.Services.Queries;
+
+namespace FaceLock.DataManagement.Services
+{
+    /// <summary>
+    /// Factory interface for creating services related to data management.
+    /// </summary>
+    public interface IDataServiceFactory
+    {
+        /// <summary>
+        /// Creates an instance of ICommandDoorLockService.
+        /// </summary>
+        /// <returns>The created instance of ICommandDoorLockService.</returns>
+        ICommandDoorLockService CreateCommandDoorLockService();
+        /// <summary>
+        /// Creates an instance of ICommandPlaceService.
+        /// </summary>
+        /// <returns>The created instance of ICommandPlaceService.</returns>
+        ICommandPlaceService CreateCommandPlaceService();
+        /// <summary>
+        /// Creates an instance of ICommandUserService.
+        /// </summary>
+        /// <returns>The created instance of ICommandUserService.</returns>
+        ICommandUserService CreateCommandUserService();
+        /// <summary>
+        /// Creates an instance of IQueryDoorLockService.
+        /// </summary>
+        /// <returns>The created instance of IQueryDoorLockService.</returns>
+        IQueryDoorLockService CreateQueryDoorLockService();
+        /// <summary>
+        /// Creates an instance of IQueryPlaceService.
+        /// </summary>
+        /// <returns>The created instance of IQueryPlaceService.</returns>
+        IQueryPlaceService CreateQueryPlaceService();
+        /// <summary>
+        /// Creates an instance of IQueryUserService.
+        /// </summary>
+        /// <returns>The created instance of IQueryUserService.</returns>
+        IQueryUserService CreateQueryUserService();
+        /// <summary>
+        /// Creates an instance of ITokenGeneratorService.
+        /// </summary>
+        /// <returns>The created instance of ITokenGeneratorService.</returns>
+        ITokenGeneratorService CreateTokenGeneratorService();
+    }
+}
---
+++ Services/FaceLock.DataManagement/ServicesImplementations/DataManagementFactory.cs
@@ -0,0 +1,83 @@
+﻿using FaceLock.DataManagement.Services.Commands;
+using FaceLock.DataManagement.Services.Queries;
+using FaceLock.DataManagement.Services;
+using FaceLock.DataManagement.ServicesImplementations.TokenGeneratorImplementation;
+using FaceLock.Domain.Repositories;
+
+namespace FaceLock.DataManagement.ServicesImplementations
+{
+    public class DataServiceFactory : IDataServiceFactory
+    {
+        private readonly IUnitOfWork _context;
+        private readonly Lazy<ICommandDoorLockService> _commandDoorLockService;
+        private readonly Lazy<ICommandPlaceService> _commandPlaceService;
+        private readonly Lazy<ICommandUserService> _commandUserService;
+        private readonly Lazy<IQueryDoorLockService> _queryDoorLockService;
+        private readonly Lazy<IQueryPlaceService> _queryPlaceService;
+        private readonly Lazy<IQueryUserService> _queryUserService;
+        private readonly Lazy<ITokenGeneratorService> _tokenGeneratorService;
+
+        public DataServiceFactory(
+            IUnitOfWork contextFactory,
+            Lazy<ITokenGeneratorService> tokenGeneratorService = null,
+            Lazy<IQueryUserService> queryUserService = null,
+            Lazy<IQueryPlaceService> queryPlaceService = null,
+            Lazy<IQueryDoorLockService> queryDoorLockService = null, 
+            Lazy<ICommandUserService> commandUserService = null,
+            Lazy<ICommandPlaceService> commandPlaceService = null,
+            Lazy<ICommandDoorLockService> commandDoorLockService = null)
+        {
+            _context = contextFactory;
+            _tokenGeneratorService = tokenGeneratorService ?? 
+                new Lazy<ITokenGeneratorService>(() => new SecureRandomTokenGeneratorStrategy());
+            _queryUserService = queryUserService ??
+                new Lazy<IQueryUserService>(() => new QueryImplementations.UserService(_context));
+            _queryPlaceService = queryPlaceService ??
+                new Lazy<IQueryPlaceService>(() => new QueryImplementations.PlaceService(_context));
+            _queryDoorLockService = queryDoorLockService ??
+                new Lazy<IQueryDoorLockService>(() => new QueryImplementations.DoorLockService(_context));
+            _commandUserService = commandUserService ??
+                new Lazy<ICommandUserService>(() => new CommandImplementations.UserService(_context));
+            _commandPlaceService = commandPlaceService ?? 
+                new Lazy<ICommandPlaceService>(() => new CommandImplementations.PlaceService(_context));
+            _commandDoorLockService = commandDoorLockService ?? 
+                new Lazy<ICommandDoorLockService>(() => 
+                new CommandImplementations.DoorLockService(_context, _tokenGeneratorService.Value));                     
+        }
+
+        public ICommandDoorLockService CreateCommandDoorLockService()
+        {
+            return _commandDoorLockService.Value;
+        }
+
+        public ICommandPlaceService CreateCommandPlaceService()
+        {
+            return _commandPlaceService.Value;
+        }
+
+        public ICommandUserService CreateCommandUserService()
+        {
+            return _commandUserService.Value;
+        }
+
+        public IQueryDoorLockService CreateQueryDoorLockService()
+        {
+            return _queryDoorLockService.Value;
+        }
+
+        public IQueryPlaceService CreateQueryPlaceService()
+        {
+            return _queryPlaceService.Value;
+        }
+
+        public IQueryUserService CreateQueryUserService()
+        {
+            return _queryUserService.Value;
+        }
+
+        public ITokenGeneratorService CreateTokenGeneratorService()
+        {
+            return _tokenGeneratorService.Value;
+        }
+    }
+}
