615e8d52f0efa0b7d4f442e21e7303ccf6e03f1b
VolodymyrPanasiuk <vovkawool@gmail.com>
2023-08-20 17:32:05 +0300

    Create service for generate Jwt token to access door lock

---
+++ Services/FaceLock.DataManagement/Services/ITokenGeneratorService.cs
@@ -0,0 +1,41 @@
+﻿using FaceLock.Domain.Entities.DoorLockAggregate;
+
+namespace FaceLock.DataManagement.Services
+{
+    /// <summary>
+    /// Interface for a token generator service that provides methods to generate JWT tokens.
+    /// </summary>
+    public interface ITokenGeneratorService
+    {
+        /// <summary>
+        /// Generates a JWT token with the given secret key.
+        /// </summary>
+        /// <param name="secretKey">The secret key used for token generation.</param>
+        /// <returns>The generated JWT token.</returns>
+        string GenerateToken(string secretKey);
+
+        /// <summary>
+        /// Generates a JWT token with the given secret key and door lock information.
+        /// </summary>
+        /// <param name="secretKey">The secret key used for token generation.</param>
+        /// <param name="doorLock">The door lock identifier.</param>
+        /// <returns>The generated JWT token.</returns>
+        string GenerateToken(string secretKey, int doorLock);
+
+        /// <summary>
+        /// Generates a JWT token with the given secret key, door lock information, and URL connection.
+        /// </summary>
+        /// <param name="secretKey">The secret key used for token generation.</param>
+        /// <param name="doorLock">The door lock identifier.</param>
+        /// <param name="urlConnection">The URL connection information.</param>
+        /// <returns>The generated JWT token.</returns>
+        string GenerateToken(string secretKey, int doorLock, string urlConnection);
+
+        /// <summary>
+        /// Generates a JWT token using the provided DoorLockSecurityInfo object.
+        /// </summary>
+        /// <param name="doorLockSecurityInfo">The security information related to the door lock.</param>
+        /// <returns>The generated JWT token.</returns>
+        string GenerateToken(DoorLockSecurityInfo doorLockSecurityInfo);
+    }
+}
---
+++ Services/FaceLock.DataManagement/ServicesImplementations/TokenGeneratorImplementation/JwtTokenGeneratorService.cs
@@ -0,0 +1,107 @@
+﻿using FaceLock.DataManagement.Services;
+using FaceLock.Domain.Entities.DoorLockAggregate;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace FaceLock.DataManagement.ServicesImplementations.TokenGeneratorImplementation
+{
+    public class JwtTokenGeneratorService : ITokenGeneratorService
+    {
+        public string GenerateToken(string secretKey)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(secretKey);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Expires = DateTime.UtcNow.AddMinutes(1),
+                IssuedAt = DateTime.UtcNow,
+                SigningCredentials = new SigningCredentials(
+                    new SymmetricSecurityKey(key),
+                    SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+
+        public string GenerateToken(string secretKey, int doorLock)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(secretKey);
+
+            var claims = new ClaimsIdentity(new[]
+            {
+                new Claim(ClaimTypes.Name, doorLock.ToString()),
+                new Claim(ClaimTypes.UserData, DateTime.UtcNow.ToString())
+            });
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = claims,
+                Expires = DateTime.UtcNow.AddMinutes(1),
+                IssuedAt = DateTime.UtcNow,
+                SigningCredentials = new SigningCredentials(
+                    new SymmetricSecurityKey(key),
+                    SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+
+        public string GenerateToken(string secretKey, int doorLock, string urlConnection)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(secretKey);
+
+            var claims = new ClaimsIdentity(new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, doorLock.ToString()),
+                new Claim(ClaimTypes.UserData, DateTime.UtcNow.ToString()),
+                new Claim(ClaimTypes.Uri, urlConnection)
+            });
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = claims,
+                Expires = DateTime.UtcNow.AddMinutes(1),
+                IssuedAt = DateTime.UtcNow,
+                SigningCredentials = new SigningCredentials(
+                    new SymmetricSecurityKey(key),
+                    SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+
+        public string GenerateToken(DoorLockSecurityInfo doorLockSecurityInfo)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(doorLockSecurityInfo.SecretKey);
+
+            var claims = new ClaimsIdentity(new[]
+{
+                new Claim(ClaimTypes.NameIdentifier, doorLockSecurityInfo.DoorLockId.ToString()),
+                new Claim(ClaimTypes.UserData, DateTime.UtcNow.ToString()),
+                new Claim(ClaimTypes.Uri, doorLockSecurityInfo.UrlConnection)
+            });
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = claims,
+                Expires = DateTime.UtcNow.AddMinutes(1),
+                IssuedAt = DateTime.UtcNow,
+                SigningCredentials = new SigningCredentials(
+                    new SymmetricSecurityKey(key),
+                    SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
