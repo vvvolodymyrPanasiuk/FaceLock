c8cf9ac9b8b79cfa7f93cb00a2070f87df1143bc
VolodymyrPanasiuk <vovkawool@gmail.com>
2023-08-20 18:09:11 +0300

    Added http medot with interaction to door lock

--- WebAPI/FaceLock.WebAPI/Controllers/RecognitionController.cs
+++ WebAPI/FaceLock.WebAPI/Controllers/RecognitionController.cs
@@ -147,5 +147,72 @@ namespace FaceLock.WebAPI.Controllers
             }
             return BadRequest(ModelState);
         }
+
+
+        // POST api/<RecognitionController>/{doorLockId}/RecognizeUserForDoorLock
+        /// <summary>
+        /// Recognize user by user's photos for door lock.
+        /// </summary>
+        /// <param name="doorLockId">The ID of the door lock to recognize.</param>
+        /// <param name="files">The files to be identification as the user.</param>
+        /// <returns>Returns status 200 (Ok) if the identification successfully or an error message.</returns>
+        /// <response code="200">Returns status 200 (Ok) if identification successfully.</response>
+        /// <response code="204">Returns status 204 (NoContent) if identification faild.</response>
+        /// <response code="400">If the model state is not valid.</response>
+        /// <response code="500">If an error occurred during the operation.</response>
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(string))]
+        [HttpPost("{doorLockId}/RecognizeUserForDoorLock")]
+        public async Task<IActionResult> RecognizeUserForDoorLock(int doorLockId, [FromForm] AddUserPhotosRequest files)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var query = _dataServiceFactory.CreateQueryDoorLockService();
+                    var doorLock = await query.GetDoorLockByIdAsync(doorLockId);
+                    var token = await query.GetAccessTokenToDoorLockAsync(doorLockId);
+
+                    var regonizeResult = new Recognition.DTO.FaceRecognitionResult<string>();
+                    foreach (var face in files.Files)
+                    {
+                        regonizeResult = await _recognitionService.RecognizeUserByFaceAsync(face);
+                    }
+
+                    if (regonizeResult.UserId == null)
+                    {
+                        return StatusCode(StatusCodes.Status204NoContent);
+                    }
+                    else
+                    {
+                        var command = _dataServiceFactory.CreateCommandDoorLockService();
+                        await command.AddDoorLockHistoryAsync(
+                            new Domain.Entities.DoorLockAggregate.DoorLockHistory
+                            {
+                                DoorLockId = doorLock.Id,
+                                UserId = regonizeResult.UserId,
+                                OpenedDateTime = DateTime.UtcNow
+                            });
+
+                        return StatusCode(StatusCodes.Status200OK, new IdentificationResponse(regonizeResult.UserId, regonizeResult.PredictionDistance));
+                    }
+                }
+                catch (ArgumentNullException ex)
+                {
+                    // Log error and return 400 response
+                    _logger.LogError($"Error: {ex.Message}");
+                    return StatusCode(StatusCodes.Status400BadRequest, $"Error: {ex.Message}");
+                }
+                catch (Exception ex)
+                {
+                    // Log error and return 500 response
+                    _logger.LogError($"Error: {ex.Message}");
+                    return StatusCode(StatusCodes.Status500InternalServerError, $"Error: {ex.Message}");
+                }
+            }
+            return BadRequest(ModelState);
+        }
     }
 }
